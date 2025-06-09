97939734ef581e720f6d515465b4362168582440
itsabhay83 <22bec003@nith.ac.in>
2025-06-09 22:56:08 +0530

    fix: comment

--- src/components/dashboard/HomePage.jsx
+++ src/components/dashboard/HomePage.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom"; // ✅ import Link
+import { Link } from "react-router-dom"; 
 
 const StudentHomePage = () => {
   const [studentName, setStudentName] = useState("");
