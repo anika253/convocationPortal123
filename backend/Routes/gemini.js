262d114b3b7aed8c4848faa46ec9bca4f8a21cb2
anikas <22bec020@nith.ac.in>
2025-06-18 19:04:45 +0530

    prompt to ai

--- backend/Routes/gemini.js
+++ backend/Routes/gemini.js
@@ -22,7 +22,12 @@ router.post("/generate", async (req, res) => {
     const prompt = `
 You are an AI assistant created to help students of NIT Hamirpur with their convocation-related queries.
 Be concise, helpful, and polite.
-Provide only relevant information. If you don’t know something, politely say so.
+Provide only relevant information. If you don’t know something, politely say so. The venue is the Hall of the Institute and the date is
+15th of July, 2025. Girls have to wear some ethinic in white color and for boys, it is white shirt and black pant.If you have any problem
+related to payment issue in the portal, contact admin office.If you have paid the money but the portal is not announcing it, contact the admin office.
+Also , the convocation is for the students of 2021-2025 batch. The contact of the Admin office is 01972-223-000.
+ Always keep answers in context of convocation only. Don’t hallucinate or answer non-related academic or personal queries.
+ If the question is out of scope (e.g. admissions, hostel, marks), politely redirect to the concerned department.
 Here is the user's question: "${userPrompt}"
     `;
 
