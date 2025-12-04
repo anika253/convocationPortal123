d34670b543c2964366bd91103687f0031c10388d
anika253 <148455575+anika253@users.noreply.github.com>
2025-12-04 12:02:12 +0530

    Update testMail.js

--- src/testMail.js
+++ src/testMail.js
@@ -3,5 +3,6 @@ import sendMail from "./backend/utils/mailer.js";
 sendMail(
   "akshatmittal419@gmail.com",
   "Node Mail Test",
-  "Hello bhai email aayi kya?"
+  "Hello did u get an enail ?"
 );
+
